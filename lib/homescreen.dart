381228c9b3fe635762335e78f6394ab4d7d0cbb6
kp0430 <122626179+kp0430@users.noreply.github.com>
2025-10-07 19:23:14 -0400

    add home screen leading to gamescreen w/animation

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,21 +1,44 @@
 import 'package:flutter/material.dart';
-
+import 'gamescreen.dart';
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: const Center(
-        child: Text('Welcome to the Home Screen!'),
-        
-      
-
-
-      
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "🎃 Kevin's Haunted Forest 🎃",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.orange),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.orange,
+                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  PageRouteBuilder(
+                  transitionDuration: const Duration(milliseconds: 1500),
+                  pageBuilder: (_, animation, __) => const HauntedForestPage(),
+                  transitionsBuilder: (_, animation, __, child) =>
+                  FadeTransition(opacity: animation, child: child),
+               ),
+    );
+              },
+              child: const Text(
+                "Enter the Haunted Forest... if you dare!",
+                style: TextStyle(fontSize: 19, color: Colors.black),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
